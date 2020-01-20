cc88bd63292a1419328860f379d7e11af17291e5
Hendrick Salomon <hsalomon86@gmail.com>
2020-01-20 13:55:59 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -18,5 +18,5 @@ def take_a_number(array, name)
   array << name
   array.each_with_index do |x, idx|
   position = idx + 1
-  puts "Welcome, #{name}. You are number " + position.to_s + " in line."
+  puts "Welcome, #{name}. You are number #{position.to_s} in line."
 end
