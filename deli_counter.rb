26e21a078d5efcf2cf2a41620dc2fded1a58b06d
Hendrick Salomon <hsalomon86@gmail.com>
2020-01-20 13:34:18 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,5 +3,11 @@ def line (array)
 
     when 0
       puts "The line is currently empty."
+    when > 0
+      arr = []
+      array.each_with_index do |x, idx|
+        position = idx + 1
+        arr << position.to_s + ". #{x}"
+        
     end 
 end
